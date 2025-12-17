a3f0fbcfa710e8f632fcb1e57b5cc0d66e100e6c
João Pedro Campos de Barcelos <97189508+Campos001@users.noreply.github.com>
2025-12-17 17:27:15 -0300

    commit

--- rabbitmq-shopping/retomar-consumers.bat
+++ rabbitmq-shopping/retomar-consumers.bat
@@ -2,7 +2,7 @@
 REM Script para retomar os consumers
 
 echo ▶️  Retomando consumers...
-docker-compose start notification-consumer analytics-consumer task-consumer
+docker-compose start notification-consumer analytics-consumer
 echo ✅ Consumers retomados!
 echo.
 echo 💡 Os consumers vão processar todas as mensagens que estavam na fila
